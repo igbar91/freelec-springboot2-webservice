ed34596ce5257e2ef1f1d72988ef65c2b4cfe996
igbar91 <iwbar91@gmail.com>
2022-07-09 01:51:49 +0900

    Chapter5 구글연동 어노테이션개선

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.jojoldu.book.springboot.web;
 
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.Dto.PostsResponseDto;
@@ -20,12 +21,12 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
         model.addAttribute("posts", postsService.findAllDesc());
 
         //userName을 Model 에 저장
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser)httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
