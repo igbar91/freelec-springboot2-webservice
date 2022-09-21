a660bcbb76165cc85617424c5efc845715bec551
igbar91 <igbar91@gmail.com>
2022-09-21 23:14:05 +0900

    부트스트랩 example템플릿 추가 및 네이버영화클론코딩 프로젝트 시작

---
+++ src/main/java/com/jojoldu/book/springboot/web/ReviewController.java
@@ -0,0 +1,34 @@
+package com.jojoldu.book.springboot.web;
+
+
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class ReviewController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/review")
+    public String review(Model model, @LoginUser SessionUser user){
+
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        //userName을 Model 에 저장
+        //SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "review";
+    }
+
+}
